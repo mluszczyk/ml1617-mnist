1c3a4981d0458342c0d84f65145f91fcd6578f81
Michał Łuszczyk <mluszczyk@users.noreply.github.com>
2017-01-17 11:23:03 +0100

    Made keras mnist demo model suitable for lib.py

--- convnet.py
+++ convnet.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, AveragePooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.layers import Reshape, BatchNormalization
+from keras.regularizers import l2
 
 
 def get_convnet_model():
